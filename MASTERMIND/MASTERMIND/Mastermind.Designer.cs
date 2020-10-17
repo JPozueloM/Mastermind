7ebb6230797db169ec43d6be49d66e2845a93e0c
Javier Pozuelo <38415516+JPozueloM@users.noreply.github.com>
2020-10-17 13:04:04 -0600

    Codigo examen

---
+++ MASTERMIND/MASTERMIND/Mastermind.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace MASTERMIND
+{
+    partial class Mastermind
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.codigo = new System.Windows.Forms.TextBox();
+            this.aciertosParciales = new System.Windows.Forms.Label();
+            this.aciertosTotales = new System.Windows.Forms.Label();
+            this.intentoActual = new System.Windows.Forms.Label();
+            this.intentosRestantes = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(141, 176);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(89, 23);
+            this.btnIniciar.TabIndex = 0;
+            this.btnIniciar.Text = "Iniciar Juego";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(141, 60);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(89, 23);
+            this.btnCheck.TabIndex = 1;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // codigo
+            // 
+            this.codigo.Location = new System.Drawing.Point(35, 62);
+            this.codigo.Name = "codigo";
+            this.codigo.Size = new System.Drawing.Size(100, 20);
+            this.codigo.TabIndex = 2;
+            // 
+            // aciertosParciales
+            // 
+            this.aciertosParciales.AutoSize = true;
+            this.aciertosParciales.Location = new System.Drawing.Point(239, 63);
+            this.aciertosParciales.Name = "aciertosParciales";
+            this.aciertosParciales.Size = new System.Drawing.Size(0, 13);
+            this.aciertosParciales.TabIndex = 3;
+            // 
+            // aciertosTotales
+            // 
+            this.aciertosTotales.AutoSize = true;
+            this.aciertosTotales.Location = new System.Drawing.Point(239, 86);
+            this.aciertosTotales.Name = "aciertosTotales";
+            this.aciertosTotales.Size = new System.Drawing.Size(0, 13);
+            this.aciertosTotales.TabIndex = 4;
+            this.aciertosTotales.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // intentoActual
+            // 
+            this.intentoActual.AutoSize = true;
+            this.intentoActual.Location = new System.Drawing.Point(245, 186);
+            this.intentoActual.Name = "intentoActual";
+            this.intentoActual.Size = new System.Drawing.Size(0, 13);
+            this.intentoActual.TabIndex = 5;
+            // 
+            // intentosRestantes
+            // 
+            this.intentosRestantes.AutoSize = true;
+            this.intentosRestantes.Location = new System.Drawing.Point(245, 206);
+            this.intentosRestantes.Name = "intentosRestantes";
+            this.intentosRestantes.Size = new System.Drawing.Size(0, 13);
+            this.intentosRestantes.TabIndex = 6;
+            // 
+            // Mastermind
+            // 
+            this.Controls.Add(this.intentosRestantes);
+            this.Controls.Add(this.intentoActual);
+            this.Controls.Add(this.aciertosTotales);
+            this.Controls.Add(this.aciertosParciales);
+            this.Controls.Add(this.codigo);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.btnIniciar);
+            this.Name = "Mastermind";
+            this.Size = new System.Drawing.Size(363, 231);
+            this.Load += new System.EventHandler(this.Mastermind_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.TextBox codigo;
+        private System.Windows.Forms.Label aciertosParciales;
+        private System.Windows.Forms.Label aciertosTotales;
+        private System.Windows.Forms.Label intentoActual;
+        private System.Windows.Forms.Label intentosRestantes;
+    }
+}
---
+++ MASTERMIND/MASTERMIND/Mastermind.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace MASTERMIND
+{
+    public partial class Mastermind : UserControl
+    {
+        private const int codigoSize = 4;
+        private const int colores = 6;
+        private const int intentosTotales = 8;
+        public  int intentosActuales = 0;
+        public int intentosRestantes1 = 8;
+
+
+
+
+
+        private Random random = new Random();
+        private bool gameStarted = false;
+        protected internal char[] CodigoSecreto = new char[4];
+
+
+
+        public Mastermind()
+        {
+      
+        InitializeComponent();
+        setupGame();
+
+      
+    }
+
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            iniciarJuego();
+        }
+
+
+        private void btnCheck_Click(object sender, EventArgs e)      
+        {
+            intentosActuales++;
+            intentosRestantes1 = intentosRestantes1 - 1;
+
+         int aciertosParciales1 = 0;
+         int aciertosTotales1 = 0;
+
+            string codigoTextoBox = codigo.Text;
+            char[] CodigoIngresado = codigoTextoBox.ToCharArray();
+
+            if(CodigoIngresado.Length > 3) { 
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (CodigoSecreto[i] == CodigoIngresado[i])
+                {
+                    aciertosTotales1++;
+                }
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                for(int j = 0; j < 4; j++) { 
+                if (CodigoSecreto[i] == CodigoIngresado[j])
+                {
+                        aciertosParciales1++;
+                }
+               }
+              }
+            }
+            else
+            {
+                MessageBox.Show("Dijite cuatro caracteres" , " Codigo incompleto", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            mostrarPista(aciertosTotales1, aciertosParciales1);
+
+            if(aciertosTotales1 == codigoSize)
+            {
+                MessageBox.Show("Completo el juego" + (intentosActuales) + " intentos!", "Ha ganado el juego", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                endGame();
+            }
+
+
+            if (intentosActuales > 7)
+            {
+                MessageBox.Show("Lo siento, no ha logrado decifrar el codigo en 8 intentos", "Ha perdido!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                endGame();
+            }
+
+        }
+
+        private void mostrarPista(int aciertosTotales1, int aciertosParciales1)
+        {
+            aciertosParciales.Text = "Aciertos parciales: " + aciertosParciales1;
+            aciertosTotales.Text = "Aciertos totales: " + aciertosTotales1;
+            intentoActual.Text = "Intento Actual: " + intentosActuales;
+            intentosRestantes.Text = "Intentos restantes: " + intentosRestantes1;
+        }
+
+        private void iniciarJuego()
+        {
+           for (int i = 0; i < 4; i++) {
+                bool repetido = false;
+                int rnd = random.Next(1, 7);
+                switch (rnd)
+                {
+                    case 1:  
+                        for (int j = 0; j < CodigoSecreto.Length; j++){
+                            if (CodigoSecreto[j] == 'A')
+                            {
+                                repetido = true;
+                            }
+                        }
+                        if (repetido == false)
+                        {
+                            CodigoSecreto[i] = 'A';
+                        }else{
+                            i = i - 1;
+                        }
+
+                        break;
+                    case 2:
+                        for (int j = 0; j < CodigoSecreto.Length; j++)
+                        {
+                            if (CodigoSecreto[j] == 'R')
+                            {
+                                repetido = true;
+                            }
+                            
+                        }
+                        if (repetido == false)
+                        {
+                            CodigoSecreto[i] = 'R';
+                        }
+                        else
+                        {
+                            i = i - 1;
+                        }
+
+                        break;
+                    case 3:
+                        for (int j = 0; j < CodigoSecreto.Length; j++)
+                        {
+                            if (CodigoSecreto[j] == 'B')
+                            {
+                                repetido = true;
+                            }
+                           
+                        }
+                        if (repetido == false)
+                        {
+                            CodigoSecreto[i] = 'B';
+                        }
+                        else
+                        {
+                            i = i - 1;
+                        }
+                        break;
+                    case 4:
+                        for (int j = 0; j < CodigoSecreto.Length; j++)
+                        {
+                            if (CodigoSecreto[j] == 'N')
+                            {
+                                repetido = true;
+                            }
+                        }
+                        if (repetido == false)
+                        {
+                            CodigoSecreto[i] = 'N';
+                        }
+                        else
+                        {
+                            i = i - 1;
+                        }
+
+                        break;
+                    case 5:
+                        for (int j = 0; j < CodigoSecreto.Length; j++)
+                        {
+                            if (CodigoSecreto[j] == 'V')
+                            {
+                                repetido = true;
+                            }
+                        }
+                        if (repetido == false)
+                        {
+                            CodigoSecreto[i] = 'V';
+                        }
+                        else
+                        {
+                            i = i - 1;
+                        }
+
+                        break;
+                    case 6:
+                        for (int j = 0; j < CodigoSecreto.Length; j++)
+                        {
+                            if (CodigoSecreto[j] == 'M')
+                            {
+                                repetido = true;
+                            }
+
+                        }
+                        if (repetido == false)
+                        {
+                            CodigoSecreto[i] = 'M';
+                        }
+                        else
+                        {
+                            i = i - 1; 
+                        }
+                        
+                        break;
+
+                }  
+            }
+          
+            //string codigo1 = CodigoSecreto.ToString();
+            //Console.WriteLine(CodigoSecreto);
+         
+            btnIniciar.Enabled = false;
+            btnCheck.Visible = true;
+            gameStarted = true;
+          
+            }
+
+
+        private void endGame()
+        {
+            gameStarted = false;
+            btnIniciar.Enabled = true;
+            btnCheck.Visible = false;
+            intentosActuales = 0; 
+
+
+
+        }
+
+
+        private void setupGame()
+        {
+            gameStarted = false;
+            btnCheck.Visible = false;
+   
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void Mastermind_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
